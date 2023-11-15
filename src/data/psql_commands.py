49f4ba00b63984c75d5b6327a0dc97ed7c85b1e3
Gonzalo Gamez <takosaga@gmail.com>
2023-11-15 15:11:29 +0200

    feat: prompting user which psql file to pass

--- src/data/psql_commands.py
+++ src/data/psql_commands.py
@@ -1,11 +1,11 @@
 import db_connection as db
 
 
-def commands():
+def commands(psql_file):
     try:
         with db.connection() as conn:
             with conn.cursor() as curs:
-                curs.execute(open("test.sql", "r").read())
+                curs.execute(open(psql_file, "r").read())
         keep = input("Do you wish to commit? (y/yes): ")
         if keep == "y" or keep == "yes":
             conn.commit()
@@ -15,4 +15,5 @@ def commands():
 
 
 if __name__ == "__main__":
-    commands
+    file = input("Which file do you want to pass thru to database? ")
+    commands(file)
